7e6fdf708662e06f0719f016d57bdd9de6b70500
annhakal <anni.l.hakala@helsinki.fi>
2025-09-15 12:28:27 +0300

    Show item list/info

--- items.py
+++ items.py
@@ -4,3 +4,17 @@ def add_item(title, ingredients, instructions, user_id):
     sql = """INSERT INTO items (title, ingredients, instructions, user_id)
              VALUES (?, ?, ?, ?)"""
     db.execute(sql, [title, ingredients, instructions, user_id])
+
+def get_items():
+    sql = "SELECT id, title FROM items ORDER BY id DESC"
+    return db.query(sql)
+
+def get_item(item_id):
+    sql = """SELECT items.title,
+                    items.ingredients,
+                    items.instructions,
+                    users.username
+                FROM items, users
+                WHERE items.user_id = users.id AND
+                items.id = ?"""
+    return db.query(sql, [item_id])[0]
