a347392068966c655a4b8e64ee69edf87f56af9c
annhakal <anni.l.hakala@helsinki.fi>
2025-10-03 17:12:00 +0300

    Store classes in database

---
+++ init.sql
@@ -0,0 +1,7 @@
+DELETE FROM  classes;
+
+INSERT INTO classes (title, value) VALUES ('Osasto', 'alkuruoka');
+INSERT INTO classes (title, value) VALUES ('Osasto', 'pääruoka');
+INSERT INTO classes (title, value) VALUES ('Osasto', 'jälkiruoka');
+INSERT INTO classes (title, value) VALUES ('Osasto', 'vegaani');
+INSERT INTO classes (title, value) VALUES ('Osasto', 'gluteeniton');
