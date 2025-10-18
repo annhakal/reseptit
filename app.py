af955368830ebc18950b85de2bb8257bf252731f
annhakal <anni.l.hakala@helsinki.fi>
2025-10-18 19:56:26 +0300

    Fix import order

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import secrets
 import sqlite3
+
 from flask import Flask
 from flask import abort, flash, make_response, redirect, render_template, request, session
 import markupsafe
+
 import config
 import db
 import items
